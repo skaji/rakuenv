c0de6f83554a26038894d395802e3e7a1fe4a916
Shoichi Kaji <skaji@cpan.org>
2018-07-28 21:43:45 +0900

    rename

--- completions/p6env.zsh
+++ completions/p6env.zsh
@@ -2,16 +2,16 @@ if [[ ! -o interactive ]]; then
     return
 fi
 
-compctl -K _rbenv rbenv
+compctl -K _p6env p6env
 
-_rbenv() {
+_p6env() {
   local words completions
   read -cA words
 
   if [ "${#words}" -eq 2 ]; then
-    completions="$(rbenv commands)"
+    completions="$(p6env commands)"
   else
-    completions="$(rbenv completions ${words[2,-2]})"
+    completions="$(p6env completions ${words[2,-2]})"
   fi
 
   reply=("${(ps:\n:)completions}")
